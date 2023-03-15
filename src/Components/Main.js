5dfdd577c537a6fdd3bf1cf1caf4beecfb896f94
Abdulrhman Al Habib <ahabib.c@tetco.sa>
2023-03-15 09:45:30 +0300

    Fixed main section position

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -6,25 +6,25 @@ import { Card } from './Card'
 export function Main() {
     return (
     <main>
-        <section className="md:flex md:items-center md:justify-center bg-green-800 overflow-visible">
-        <div className="md:flex-col">
+        <section className="md:grid grid-cols-2 items-center bg-green-800 px-20">
+        <div className="md:flex-col py-10 px-20 relative left-1/3">
             <h1>Little Lemon</h1>
             <h3>Chicago</h3>
             <p className="flex-initial w-80 py-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             <PrimaryButton name="TEST" />
         </div>
-        <img src={RestaurantImage} alt="restaurant food sample" className="h-80 pt-7 pl-6 relative"/>
+        <img src={RestaurantImage} alt="restaurant food sample" className="rounded h-80 md:relative right-1 top-1/4"/>
         </section>
-        <section className="md:flex-col md:items-center md:justify-center">
+        <section className="md:flex-col md:items-center md:justify-center p-20">
             <div className="md:flex md:justify-between py-10 px-20">
                 <h2>This weeks specials!</h2>
                 <PrimaryButton name="TEST" />
             </div>
-            <div className="grid md:grid-cols-3 gap-10 py-10 px-20">
+            <div className="grid md:grid-cols-3 gap-10 py-10 px-20 mx-20">
                 <Card name="Greek Salad" />
                 <Card name="Bruchetta" />
                 <Card name= "Lemon Dessert" />
-            </div>   
+            </div>
         </section>
     </main>
     )
