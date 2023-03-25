ff349825a55a7065d5b890920a37ce8b1c54085e
AALHABIB <aalhabib9774@gmail.com>
2023-03-25 16:01:01 +0300

    Set header/footer to global

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom'
 import { HomePage } from './Pages/HomePage';
 import { BookingPage } from './Pages/BookingPage';
+import { Header } from './Components/Header';
+import { Footer } from './Components/Footer';
 
 
 function App() {
   return (
     <>
+      <Header />
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/booking' element={<BookingPage />} />
       </Routes>
+      <Footer />
     </>
   );
 }
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,15 +1,11 @@
-import { Header } from "../Components/Header"
-import { Footer } from "../Components/Footer"
 import { CallToAction } from "../Components/CallToAction"
 import { Specials } from "../Components/Specials"
 
 export function HomePage() {
     return (
         <>
-            <Header />
             <CallToAction />
             <Specials />
-            <Footer />
         </>
     )
 }
