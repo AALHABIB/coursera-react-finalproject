0c8f81bcc25dd9689827e040a010e7591609afea
Abdulrahman Alhabib <abdulrahmanalhabib@Abdulrahmans-MacBook-Air.local>
2023-03-25 15:39:42 +0300

    Fixed Specials component routing

--- src/Components/Specials.js
+++ src/Components/Specials.js
@@ -1,7 +1,20 @@
+import { Card } from './Card'
+import { PrimaryButton } from './PrimaryButton'
 
 export function Specials() {
     return (
-        <>
-        </>
+        
+        <section className="md:flex-col md:items-center md:justify-center p-20">
+             <div className="md:flex md:justify-between py-10 px-20">
+                 <h2>This weeks specials!</h2>
+                 <PrimaryButton name="TEST" />
+             </div>
+             <div className="grid md:grid-cols-3 gap-10 py-10 px-20 mx-20">
+                 <Card name="Greek Salad" />
+                 <Card name="Bruchetta" />
+                 <Card name= "Lemon Dessert" />
+             </div>
+         </section>
+        
     )
 }
